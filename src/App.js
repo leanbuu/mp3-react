0119cfd0d58b1ec87ced4a24a17d2018537bc8ed
Damian Hoang <vas2k4xd@gmail.com>
2024-10-09 01:31:52 +0930

    Test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './component/Dashboard';
+import NotFound404 from './component/NotFound404';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Học Lập Trình
-        </p>
-      </header>
-    </div>
+return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/Dashboard">Dashboard</Link></li>
+            
+          </ul>
+        </nav>
+
+        {/* Định nghĩa các route */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/Setting.jsx
@@ -0,0 +1,6 @@
+function Setting(){
+    return(
+    <h1>Welcome to Setting page</h1>
+    )
+    }
+    export default Setting;
